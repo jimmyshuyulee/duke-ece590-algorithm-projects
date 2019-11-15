bff55a869ee8e2ae503eba9e44725ba177a3ec6b
jimmy <jimmyjoy850812@gmail.com>
2019-11-15 18:23:55 -0500

    added comments

--- ece590_project3/project3.py
+++ ece590_project3/project3.py
@@ -3,9 +3,9 @@ Math 590
 Project 3
 Fall 2019
 
-Partner 1:
-Partner 2:
-Date:
+Partner 1: Shu Yu Lee
+Partner 2: Fang Feng
+Date: 11/15/2019
 """
 
 # Import math and p3tests.
@@ -16,9 +16,23 @@ from p3tests import *
 
 """
 detectArbitrage
+output a single list of vertex ranks corresponding to the negative cost cycle
+
+INPUTS
+currencies: a 2D list of the adjacency matrix representing the -log() value
+of currency exchange rates
+tol: the tolerance value for errors coming form decimal representation of
+float numbers
+
+OUTPUTS:
+neg_cycle: a list representing the negative cost cycle in currencies
 """
 def detectArbitrage(currencies, tol=1e-15):
     ##### Your implementation goes here. #####
+
+    # Abstract the distance update portion of the code into a function,
+    # since it will be used in the normal shortest path finding iterations
+    # and the additional round to look for negative cost cycle
     def updateDist():
         for u in currencies.adjList:
             for neighbor in u.neigh:
@@ -31,22 +45,30 @@ def detectArbitrage(currencies, tol=1e-15):
     currencies.adjList[0].dist = 0;
     for i in range(len(currencies.adjList)-1):
         updateDist()
+    # Distnce of every nodes after V-1 iterations
     correct_dist = [v.dist for v in currencies.adjList]
     updateDist()
+    # Distnce of every nodes after V iterations
     new_dist = [v.dist for v in currencies.adjList]
 
     neg_cycle = []
     for i in range(len(correct_dist)):
+        # look for the vertex which has its distance changes in the
+        # additional iteration
         if correct_dist[i] - new_dist[i] > tol:
             current_vertex = currencies.adjList[i]
+            # Traverse back to find the cycle
             while not current_vertex.rank in neg_cycle:
                 neg_cycle.append(current_vertex.rank)
                 current_vertex = current_vertex.prev
+            # since our starting vertex may not be in the cycle, look for
+            # the start of the cycle and cut off additional edges
             for j in range(len(neg_cycle)):
                 if neg_cycle[j] == current_vertex.rank:
                     neg_cycle = neg_cycle[j:]
                     neg_cycle.append(current_vertex.rank)
-                    return neg_cycle[::-1]
+                    # Make sure is in the correct order
+                    return neg_cycle[::-1] 
     return neg_cycle
     ##### Your implementation goes here. #####
 
@@ -54,6 +76,13 @@ def detectArbitrage(currencies, tol=1e-15):
 
 """
 rates2mat
+return the adjacency matrix with the correctly weighted edges
+
+INPUTS
+rates: a 2D list of currency exchage rates
+
+OUTPUTS
+a 2D list of the correct adjacency matrix.
 """
 def rates2mat(rates):
     ##### Your implementation goes here. #####
